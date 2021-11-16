fa16e6b5f4d7b0ed0058aa7774b57eb8f56c9685
Tamim-uddin <muhammadtamimuddin@gmail.com>
2021-11-16 23:53:28 +0600

    useradded

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -5,38 +5,79 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import './Booking.css';
-import { TextField } from '@mui/material';
+import { Alert, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 import UseAuth from '../Hooks/UseAuth';
 
 const Booking = () => {
     const { _id } = useParams(); 
-    const [bookings, setbookings] = useState({});
+    const [tours, settours] = useState({});
     const {user} = UseAuth();
+    const initialInfo = {Username: user.displayName, email: user.email}
+    const [bookingInfo, setbookingInfo] = useState(initialInfo);
+    
 
     useEffect( () => {
         fetch(`http://localhost:5000/tours/${_id}`)
         .then(res => res.json())
-        .then(data => setbookings(data))
-    } , [])
+        .then(data => settours(data))
+    } , []);
+
+
+
+    const handleonBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newInfo = {bookingInfo};
+        newInfo[field] = value;
+        console.log(newInfo, value, field);
+        setbookingInfo(newInfo);
+    }
+
+    const handleonSubmit = e => {
+        e.preventDefault();
+        const booking = {
+            ...bookingInfo,
+            tourName: tours.name,
+            price: tours.price
+        }
+
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+        .then(res => res.json)
+        .then(data => {
+            if(data.insertedId){
+                alert('Bookking successfull');
+                
+            }
+            
+        })
+    }
 
 
     return (
         <div className="booking">
+           
            <Grid container spacing={2}>
                 <Grid item xs={12} md={8}>
                     <Card sx={{ minWidth: 275 }}>
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                {bookings.img}
+                               <img style={{width: '90%'}} src={tours.img} alt="" />
                             </Typography>
                             <Typography variant="h5" component="div">
-                                {bookings.name}
+                                {tours.name}
                             </Typography>
                             <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                             ${bookings.price}
+                             ${tours.price}
                             </Typography>
                             <Typography variant="body2">
-                                {bookings.des}
+                                {tours.des}
                           
                             </Typography>
                         </CardContent>
@@ -44,11 +85,14 @@ const Booking = () => {
                     </Card>
                 </Grid>
                 <Grid item xs={12} md={4}>
-                    <form>
+              
+                    <form onSubmit={handleonSubmit}>
                         <TextField 
                         sx={{width: '75%', mb: 2}}
                         id="standard-basic" 
                         label="Name" 
+                        name="Username"
+                        onBlur={handleonBlur}
                         defaultValue={user.displayName}
                         variant="standard" /> <br />
                         <TextField 
@@ -56,28 +100,23 @@ const Booking = () => {
                         id="standard-basic" 
                         label="Email" 
                         type="email"
+                        name="email"
+                        onBlur={handleonBlur}
                         defaultValue={user.email}
                         variant="standard" /> <br />
-                       
-                        
-                        <TextField 
-                        sx={{width: '75%', mb: 2}}
-                        id="standard-basic" 
-                        label="Service Name" 
-                        defaultValue={bookings.name}
+                        <TextField
+                        sx={{width: '75%', mb: 2}} 
+                        disabled                    
+                        label="Tour Name"
+                        defaultValue={tours.name}
                         variant="standard" /> <br />
-                        <TextField 
-                        sx={{width: '75%', mb: 2}}
-                        id="standard-basic" 
-                        label="Phone" 
-                        defaultValue=''
+                        <TextField
+                        disabled
+                        sx={{width: '75%', mb: 2}}                        
+                        label="Price"
+                        defaultValue={tours.price}
                         variant="standard" /> <br />
-                        <TextField 
-                        sx={{width: '75%', mb: 2}}
-                        id="standard-basic" 
-                        label="Address" 
-                        defaultValue=''
-                        variant="standard" /> <br />
+                        <Button type="submit" variant="contained">Place Order</Button>
                     </form>
                 </Grid>
                 
--- src/Pages/MyOrders/MyOrders.js
+++ src/Pages/MyOrders/MyOrders.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import UseAuth from '../Hooks/UseAuth';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const MyOrders = () => {
+    const [Orders, setOrders] = useState([]);
+    const {user} = UseAuth();
+
+    useEffect( () => {
+        fetch(`http://localhost:5000/bookings?email=${user.email}`)
+        .then(res => res.json())
+        .then(data => setOrders(data))
+    } , [])
     return (
         <div>
-            <h3>this is from my orders</h3>
+            <h3>this is from my orders {Orders.length}</h3>
+
+            <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                           {Orders.Username}
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                             {Orders.tourName}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            {Orders.price}
+                        </Typography>
+                        <Typography variant="body2">
+                        well meaning and kindly.
+                        
+                        </Typography>
+                    </CardContent>
+                    
+            </Card>
+            
         </div>
     );
 };
