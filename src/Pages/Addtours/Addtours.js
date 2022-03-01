ebf6518a47ec0e4f5dbcb0fe59a69966e2ea12f9
Tamim-uddin <muhammadtamimuddin@gmail.com>
2022-03-01 15:24:20 +0600

    add tour responsive

--- src/Pages/Addtours/Addtours.js
+++ src/Pages/Addtours/Addtours.js
@@ -1,4 +1,4 @@
-import { Button, CircularProgress } from '@mui/material';
+import { Button, CircularProgress, Grid, Paper } from '@mui/material';
 import { Box } from '@mui/system';
 import axios from 'axios';
 import React from 'react';
@@ -18,21 +18,22 @@ const Addtours = () => {
         })
     };
     return (
-        <Box>
-        <div className="tour-added">
-            <h1 style={{fontFamily:'"Playfair Display",serif', marginBottom: '25px'}}>Add Tour Event</h1>
+        <Grid sx={{mt: '40px'}}>
+         <Paper elevation={10} style={{padding: '20px', height: '70vh', width: 300, margin: 'auto', border: '1px solid #804d4d', backgroundColor: ' #c69f9f'}}>
+             <Grid align="center">
+            <h1 style={{fontFamily:'"Playfair Display",serif', marginBottom: '25px'}}>Add Tour Event</h1></Grid>
             <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("img")} placeholder="Img Url"/> <br />
-            <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" /> <br />
-            <input {...register("loc", { required: true, maxLength: 20 })} placeholder="Location" /> <br />
-            <input type="number" {...register("price")} placeholder="Price" /> <br />
-            <input type="number" {...register("age")} placeholder="age" /> <br />
-            <input type="number" {...register("daytime")} placeholder="Daytime" /> <br />
-            <textarea {...register("des")} placeholder="Description"/> <br />< br />
+            <input {...register("img")} placeholder="Img Url" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
+            <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
+            <input {...register("loc", { required: true, maxLength: 20 })} placeholder="Location" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
+            <input type="number" {...register("price")} placeholder="Price" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
+            <input type="number" {...register("age")} placeholder="age" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
+            <input type="number" {...register("daytime")} placeholder="Daytime" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
+            <textarea {...register("des")} placeholder="Description" style={{width: '100%', marginBottom: '3px', border: '1px solid #909195', borderRadius: '5px'}}/> <br />
             <Button type='submit' variant='contained' sx={{backgroundColor: '#804d4d'}} >Add Tour </Button>
             </form>
-        </div>
-        </Box>
+        </Paper>
+        </Grid>
     );
 };
 
