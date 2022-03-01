dc3de39b5a88238b075b85084500299034841904
Tamim-uddin <muhammadtamimuddin@gmail.com>
2022-03-01 13:33:21 +0600

    login responsive

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,4 +1,4 @@
-import { Alert, Box, Button, Card, CardContent, CircularProgress, Grid, TextField } from '@mui/material';
+import { Alert, Avatar, Box, Button, Card, CardContent, CircularProgress, Grid, Paper, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router';
 import { NavLink } from 'react-router-dom';
@@ -7,6 +7,7 @@ import bg from '../../../Images/background/bg6.jpg';
 import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
 import GoogleIcon from '@mui/icons-material/Google';
 import { FcGoogle } from "react-icons/fc";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import './Login.css';
 
 
@@ -43,41 +44,45 @@ const Login = () => {
 
 
     return (     
-        <Box>
-                <Grid container spacing={2} sx={{ boxShadow: 3, width: '25%', height: '420px', mt: '130px', ml: '500px', p: '30px', border: '1px solid 	#804d4d', borderRadius: '5px'}}>
-                {/* <Grid item xs={12} sm={6} md={6} >
-                    <img src={bg} alt=''/>
-                </Grid>   */}
-                
-                <Grid item xs={12} sm={6} md={12} sx={{textAlign: 'center', mt: '20px'}}>
-                <h1 style={{fontFamily:'"Playfair Display",serif'}}>Please Login</h1>
+      
+                <Grid sx={{mt: '40px'}}>
+                    <Paper elevation={10} style={{padding: '20px', height: '70vh', width: 280, margin: 'auto', border: '1px solid #804d4d'}}>
+                <Grid  align="center">
+                <Avatar sx={{backgroundColor: '#804d4d'}}><LockOutlinedIcon /></Avatar>
+                <h1 style={{fontFamily:'"Playfair Display",serif', marginTop: '20px', color:'#303030'}}>Please Login</h1>
+                </Grid>
                     <form onSubmit={handleOnSubmit}>
                     <TextField 
-                    sx={{width: '100%'}}
+                    fullWidth
                     label="Your Email" 
                     name="email"
                     type="email"
                     onBlur={handleOnBlur}
                     variant="standard" /> <br />
                     <TextField 
-                    sx={{width: '100%', mb: '5px'}}
+                    fullWidth
                     label="Your Password" 
                     name="password"
                     type="password"
                     onBlur={handleOnBlur}
                     variant="standard" /> <br />
-                    <Button type='submit' variant='contained'  sx={{fontSize: 'medium', backgroundColor: '#804d4d'}}>Login<LoginRoundedIcon/></Button><br />
+                    <Button type='submit' variant='contained'  sx={{fontSize: 'medium', backgroundColor: '#804d4d', mt: '5px'}}>Login<LoginRoundedIcon/></Button><br />
                     </form> <br />
                     {/* google sign in */}
                     <Button onClick={handlegooglelogin} type="submit"  variant='contained'  sx={{fontSize: '15px', backgroundColor: '#804d4d'}}><FcGoogle sx={{fontSize: '35px', mr: '10px'}}/>Google Sign</Button><br />
-                    <NavLink to="/register" style={{color: '#804d4d'}}>New User?Please Register</NavLink> <br /> 
-                </Grid>
-            </Grid>
-            {user.email && <Alert severity="success" sx={{width: '75%'}}>Successfully Login</Alert>};
+                    
+                    <NavLink to="/register" style={{color: '#804d4d'}}>New User?Please Register</NavLink><br /> 
+                
+                </Paper>
+                {user.email && <Alert severity="success" sx={{width: '75%'}}>Successfully Login</Alert>};
             {error && <Alert severity="error" sx={{width: '75%'}}>{error}</Alert>}
-        </Box>
-       
         
+            </Grid>
+            
+       
+    //    sx={{ boxShadow: 3, width: '35%', height: '420px',margin: 'auto', p: '30px', border: '1px solid 	#804d4d', borderRadius: '5px'}}
+    // item xs={12} sm={6} md={12}
+    // container spacing={2}
     );
 };
 
